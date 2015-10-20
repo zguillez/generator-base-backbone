c0e8dc2f13b4a31fc95a465a7bfe002e133cb623
Guillermo de la Iglesia <zguillez@gmail.com>
2015-10-20 10:42:34 +0200

    Fix JSHint

--- generators/app/templates/Gruntfile.js
+++ generators/app/templates/Gruntfile.js
@@ -13,13 +13,13 @@ module.exports = function(grunt) {
 	require('./grunt/watch')(grunt);
 	require('./grunt/reload')(grunt);
 	grunt.registerTask('default', ['serve']);
-	grunt.registerTask('serve', function(env) {
+	grunt.registerTask('serve', function() {
 		grunt.task.run(['clean', 'jshint', 'uglify', 'jade', 'sass', 'copy', 'connect', 'open', 'watch']);
 	});
-	grunt.registerTask('build', function(env) {
+	grunt.registerTask('build', function() {
 		grunt.task.run(['clean', 'jshint', 'uglify', 'jade', 'sass', 'copy']);
 	});
-	grunt.registerTask('rebuild', function(env) {
+	grunt.registerTask('rebuild', function() {
 		grunt.task.run(['clean', 'uglify', 'jade', 'sass', 'copy']);
 	});
 };
