1a4ff8fc9fa9e46c2c8f3c5e8f22abe58796ebcb
Guillermo de la Iglesia <zguillez@gmail.com>
2015-11-09 12:53:49 +0100

    Delete modules

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,30 +3,30 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 module.exports = yeoman.generators.Base.extend({
-  prompting: function() {
-    var done = this.async();
-    this.log(yosay('Welcome to the zetadelic ' + chalk.red('BaseBackbone v1.0.1') + ' generator!'));
-    var prompts = [];
-    this.prompt(prompts, function(props) {
-      this.props = props;
-      done();
-    }.bind(this));
-  },
-  writing: {
-    app: function() {
-      this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
-      this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
-      this.fs.copy(this.templatePath('Gruntfile.js'), this.destinationPath('Gruntfile.js'));
-      this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
-      this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
-    },
-    projectfiles: function() {
-      this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
-      this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
-      this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
-    }
-  },
-  install: function() {
-    this.installDependencies();
-  }
+	prompting: function() {
+		var done = this.async();
+		this.log(yosay('Welcome to the zetadelic ' + chalk.red('BaseBackbone v1.0.3') + ' generator!'));
+		var prompts = [];
+		this.prompt(prompts, function(props) {
+			this.props = props;
+			done();
+		}.bind(this));
+	},
+	writing: {
+		app: function() {
+			this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
+			this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
+			this.fs.copy(this.templatePath('Gruntfile.js'), this.destinationPath('Gruntfile.js'));
+			this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
+			this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
+		},
+		projectfiles: function() {
+			this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
+			this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
+			this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
+		}
+	},
+	install: function() {
+		this.installDependencies();
+	}
 });
