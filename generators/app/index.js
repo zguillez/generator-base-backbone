ac6c066fe374d97cc80df4c1ae5ce363de714db1
Guillermo de la Iglesia <zguillez@gmail.com>
2016-06-20 11:55:04 +0200

    yeoman fixes

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,24 +10,19 @@ module.exports = yeoman.Base.extend({
 		var done = this.async();
 		this.log(yosay('Welcome to the zetadelic ' + chalk.red('BaseBackbone v' + this.pkg.version) + ' generator!'));
 		var prompts = [];
-		this.prompt(prompts, function(props) {
+		return this.prompt(prompts).then(function(props) {
 			this.props = props;
-			done();
 		}.bind(this));
 	},
 	writing: {
-		app: function() {
-			this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
-			this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
-			this.fs.copy(this.templatePath('Gruntfile.js'), this.destinationPath('Gruntfile.js'));
-			this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
-			this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
-		},
-		projectfiles: function() {
-			this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
-			this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
-			this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
-		}
+		this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
+		this.fs.copy(this.templatePath('jshintrc'), this.destinationPath('.jshintrc'));
+		this.fs.copy(this.templatePath('bowerrc'), this.destinationPath('.bowerrc'));
+		this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
+		this.fs.copy(this.templatePath('bower.json'), this.destinationPath('bower.json'));
+		this.fs.copy(this.templatePath('Gruntfile.js'), this.destinationPath('Gruntfile.js'));
+		this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
+		this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
 	},
 	install: function() {
 		this.installDependencies();
