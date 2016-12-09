d8d43492b7754a874b853ba982e68c4a1f843d30
icosa <icosahedron@hkcnews.com>
2016-12-09 12:00:53 +0800

    fix rewriting data.json so that it reflects users' options

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,6 +5,30 @@ var yosay = require('yosay');
 var fs = require('fs');
 var replace = require('replace');
 
+
+// var _ = require('underscore'),
+//     fs = require('fs'),
+//     data = JSON.parse(fs.readFileSync('./data.json', 'utf-8')).data;
+
+// _.each(data, function(item) {
+
+//   var newJson = item[0],
+//       files = item[1];
+
+//   _.each(files, function(file) {
+//     var path = '.' + file,
+//         oldJson = JSON.parse(fs.readFileSync(path, 'utf-8')),
+//         newJson = _.extend(oldJson, newJson),
+//         printableJson = JSON.stringify(newJson, null, 2);
+
+//     fs.writeFile(path, printableJson, function(err) {
+//       if (err) throw('File save error: '+ err);
+//       console.log('file saved');
+//     });
+
+//   });
+
+// });
 module.exports = yeoman.Base.extend({
 
   initializing: function () {
@@ -50,17 +74,28 @@ module.exports = yeoman.Base.extend({
       css:this.props.css.toLowerCase()
     });
 
+    // icosa:
+    // src files are using a syntax that contains a keyword collision with copyTpl, namely this <%= =>
+    //  - copy duplicated files and then delete the unwanted one,
+    //  - read and rewrite the target file
+    // are workarounds that i came up with to make things work for the meantime
     this.fs.copy(this.templatePath('grunt'), this.destinationPath('grunt'));
     this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
     this.fs.delete(this.destinationPath('styles/main.'+( this.props.css == 'LESS' ? "sass" : "less")));
     this.fs.delete(this.destinationPath('grunt/'+( this.props.css == 'LESS' ? "sass" : "less") + '.js'));
-    // this.fs.extendJSON(this.destinationPath('src/data/data.json'), [[{
-    //   "name": "Bootstrap",
-    //   "url": "http://getbootstrap.com/"
-    // }, {
-    //   "name": "LESS",
-    //   "url": "http://lesscss.org/"
-    // }]]);
+
+    var libData = this.fs.readJSON(this.destinationPath('src/data/data.json'));
+    var i = 0;
+    for (i; i<libData.length; i++){
+      if(this.props.bootstrap == '4' && libData[i].name == 'Bootstrap'){
+        libData[i].name = 'Bootstrap v4';
+        libData[i].url = 'https://v4-alpha.getbootstrap.com/';
+      }else if(this.props.css == 'LESS' && libData[i].name ==  'Sass'){
+        libData[i].name = 'Less';
+        libData[i].url = 'http://lesscss.org/';
+      }
+    }
+    this.fs.writeJSON(this.destinationPath('src/data/data.json'), libData);
   }
   ,
   install: function () {
