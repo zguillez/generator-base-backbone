7ddc8a82ec12d5825737ebe4049daff2198fae14
Guillermo de la Iglesia <zguillez@gmail.com>
2016-12-21 19:10:41 +0100

    merge

--- gulpfile.js
+++ gulpfile.js
@@ -15,18 +15,18 @@ gulp.task('nsp', function (cb) {
 });
 gulp.task('pre-test', function () {
 	return gulp.src('generators/**/*.js').pipe(excludeGitignore()).pipe(istanbul({
-			includeUntested: true
-		})).pipe(istanbul.hookRequire());
+		includeUntested: true
+	})).pipe(istanbul.hookRequire());
 });
 gulp.task('test', ['pre-test'], function (cb) {
 	var mochaErr;
 	gulp.src('test/**/*.js').pipe(plumber()).pipe(mocha({
-			reporter: 'spec'
-		})).on('error', function (err) {
-			mochaErr = err;
-		}).pipe(istanbul.writeReports()).on('end', function () {
-			cb(mochaErr);
-		});
+		reporter: 'spec'
+	})).on('error', function (err) {
+		mochaErr = err;
+	}).pipe(istanbul.writeReports()).on('end', function () {
+		cb(mochaErr);
+	});
 });
 gulp.task('watch', function () {
 	gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
